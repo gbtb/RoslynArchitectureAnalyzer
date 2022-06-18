7c80417a062dbc49ba407a7643880ab58eeab8dc
gbtb <goodbetterthebeast3@gmail.com>
2022-06-18 21:11:28 +1000

    trying compilationend

--- src/Roslyn.Architecture.Analyzer/DependencyAnalyzer.cs
+++ src/Roslyn.Architecture.Analyzer/DependencyAnalyzer.cs
@@ -22,7 +22,12 @@ public class DependencyAnalyzer: DiagnosticAnalyzer
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
         context.EnableConcurrentExecution();
-        context.RegisterCompilationAction(AnalyzeReferences);
+#pragma warning disable RS1013
+        context.RegisterCompilationStartAction(startContext =>
+#pragma warning restore RS1013
+        {
+            startContext.RegisterCompilationEndAction(AnalyzeReferences);
+        });
     }
 
     private void AnalyzeReferences(CompilationAnalysisContext ctx)
